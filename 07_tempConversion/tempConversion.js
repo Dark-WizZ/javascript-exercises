863ec436150c7f5820a705d30b964e61873b3d76
Dark-WizZ <mailwithakram@gmail.com>
2022-08-03 18:59:49 +0530

    Pass 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  return Math.round(((f-32)/1.8)*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  return Math.round(((c+40)*1.8-40)*10)/10;
 };
 
 // Do not edit below this line
