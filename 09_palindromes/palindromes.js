b66a3e1e7f68dcf16b427182ed68a28db6c6bf79
Dark-WizZ <mailwithakram@gmail.com>
2022-08-03 20:49:05 +0530

    Pass palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let sorted = str.toLowerCase().split('').filter(s => s>='a' && s<='z');
+  for(let i=0; i<sorted.length/2; i++){
+    if(sorted[i]!=sorted[sorted.length-i-1]) return false
+  }
+  return true;
 };
 
 // Do not edit below this line
