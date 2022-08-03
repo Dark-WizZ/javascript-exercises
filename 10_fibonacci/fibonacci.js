676200353adce26758d264edd89f643a1238e588
Dark-WizZ <mailwithakram@gmail.com>
2022-08-03 21:39:43 +0530

    Pass 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n<=0) return 'OOPS';
+  let prepre = 0;
+  let pre = 1;
+  for(let i=1; i<n; i++){
+    let temp = pre;
+    pre = pre+prepre;
+    prepre = temp;
+  }
+  return pre;
 };
 
 // Do not edit below this line
