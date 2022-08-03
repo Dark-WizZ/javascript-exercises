5c7d60aed6637e58c43541707d179eb45c4e21c0
Dark-WizZ <mailwithakram@gmail.com>
2022-08-04 01:56:24 +0530

    Complete 13_caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,31 @@
-const caesar = function() {
-
+const caesar = function(str, inc) {
+  let res = '';
+  inc = inc%26;
+  for(let i=0; i<str.length; i++){
+    if(/\W/.test(str.charAt(i))){
+      res += str.charAt(i);
+    }else{
+      let code = str.charCodeAt(i)+inc;
+      if(code-inc>=65 && code-inc<=90){
+        if(code>90){
+          code = 64+(code-90)
+        }
+        if(code<65){
+          code = 91-(65-code)
+        }
+      }
+      if(code-inc>=97 && code-inc<=122){
+        if(code>122){
+          code = 96+(code-122)
+        }
+        if(code<96){
+          code = 123-(96-code)
+        }
+      }
+      res += String.fromCharCode(code);
+    }
+  }
+  return res;
 };
 
 // Do not edit below this line
