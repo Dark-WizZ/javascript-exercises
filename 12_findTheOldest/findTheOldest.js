d15644f9e9d01a9cc030799199ab1cb64e6bc9a2
Dark-WizZ <mailwithakram@gmail.com>
2022-08-04 00:20:37 +0530

    Pass 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let older = arr[0];
+  for(let person of arr){
+    if(person.yearOfDeath==undefined){
+      person.yearOfDeath = new Date().getFullYear();
+    }
+    if((person.yearOfDeath-person.yearOfBirth)>(older.yearOfDeath-older.yearOfBirth)){
+      older = person;
+    }
+  }
+  return older;
 };
 
 // Do not edit below this line
